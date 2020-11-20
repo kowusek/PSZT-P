841187e964d0fdbd3ab0c11bcda13ba9faa0baa5
Jakub Rybalko <rybalkojakubpiotr@gmail.com>
2020-11-20 20:11:06 +0100

    created basic main

---
+++ include/gen_algorithm.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <random>
+#include <chrono>
+#include <vector>
+
+class gen_algorithm {
+private:
+    void cross();
+    void mutate();
+
+    unsigned population_size;
+    float mutation_probability;
+    float cross_probability;
+    unsigned parm_t;
+    std::vector<std::vector<unsigned>>* population;
+    std::vector<unsigned>* best_so_far;
+
+public:
+    gen_algorithm();
+
+    ~gen_algorithm();
+
+    gen_algorithm(unsigned population_size, float mutation_probability, float cross_probability, unsigned t);
+
+    void gen_function();
+
+    unsigned generate_number();
+};
---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "gen_algorithm.hpp"
+
+int main() {
+
+    int populationSize = 0;
+    float mutationPropability = 0;
+    float crossPropability = 0;
+
+    //gen_algorithm algorithm;
+
+    do
+    {
+        std::cout << "Podaj rozmiar populacji: " << std::endl;
+        std::cin >> populationSize;
+        if(populationSize < 1) std::cout << "Rozmiar populacji musi byc wiekszy niz 0" << std::endl;
+
+    } while (populationSize < 1);
+    
+    do
+    {
+        std::cout << "Podaj prawdopodobienstwo mutacji: " << std::endl;
+        std::cin >> mutationPropability;
+        if(mutationPropability < 0 || mutationPropability > 100) std::cout << "Prawdopodobienstwo musi byc z zakresu 0-100" << std::endl;
+        
+    } while (mutationPropability < 0 || mutationPropability > 100);
+
+    do
+    {
+        std::cout << "Podaj prawdopodobienstwo krzyzowania: " << std::endl;
+        std::cin >> crossPropability;
+        if(crossPropability < 0 || crossPropability > 100) std::cout << "Prawdopodobienstwo musi byc z zakresu 0-100" << std::endl;
+        
+    } while (crossPropability < 0 || crossPropability > 100);
+
+    //algorithm.gen_function(populationSize, mutationPropability, crossPropability);
+
+}
